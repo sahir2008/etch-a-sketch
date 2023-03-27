05af089061091c70adc561497dc5b255fe3079ca
Sahir Mohsen <sahirmohsen@gmail.com>
2023-03-27 17:21:01 +0200

    add reset button that resize the grid

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const container = document.querySelector(".container");
+const fragment = document.createDocumentFragment();
 
-function gridMaker(numOfGrid = 16, containerWidth = 500) {
+function hoverEffect() {
+  const squares = document.querySelectorAll(".square-div");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "red";
+    });
+  });
+}
+
+function gridMaker(numOfGrid, containerWidth = 500) {
   for (let i = 0; i < numOfGrid * numOfGrid; i++) {
     let div = document.createElement("div");
     container.style.width = `${containerWidth}px`;
     div.classList.add("square-div");
     div.style.setProperty("--myCellWidth", `${containerWidth / numOfGrid}px`);
     div.style.setProperty("--myCellHeight", `${containerWidth / numOfGrid}px`);
-    container.appendChild(div);
+    fragment.appendChild(div);
+  }
+  container.appendChild(fragment);
+  hoverEffect();
+}
+let userSelection =16;
+
+function handleClick() {
+  let getDataFromUser = prompt("Enter A number from 1 to 100");
+  userSelection = parseInt(getDataFromUser);
+  if (!isNaN(userSelection) && userSelection >= 1 && userSelection <= 100) {
+    // Clear the container before creating a new grid
+    container.innerHTML = "";
+    gridMaker(userSelection);
+  } else {
+    alert("Please enter a valid number between 1 and 100.");
   }
 }
-gridMaker();
 
-const squares = document.querySelectorAll(".square-div");
-squares.forEach((square) => {
-  square.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "red";
-  });
-});
+
+gridMaker(userSelection);
